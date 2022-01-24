06fe32f1b308ea74293d58cbb2d12c1fdf0256b7
blopez <borja.lopez@stratusgrid.com>
2022-01-24 16:51:12 +0100

    First commit

--- -inputs.tf
+++ -inputs.tf
@@ -1,6 +1,25 @@
-variable "name" {
-  description = "name to prepend to all resource names within module"
+variable "email" {
+  description = "The email address to subscribe for SNS limit alert messages, leave blank if SNS alerts not needed."
   type        = string
+  default     = ""
+}
+
+variable "accountlist" {
+  description = "List of comma-separated and double-quoted account numbers to monitor. If you leave this parameter blank, the solution will only monitor limits in the primary account. If you enter multiple secondary account IDs, you must also provide the primary account ID in this parameter."
+  type        = string
+  default     = ""
+}
+
+variable "slackhookurl" {
+  description = "SSM parameter key for incoming Slack web hook URL. Leave blank if you do not wish to receive Slack notifications."
+  type        = string
+  default     = ""
+}
+
+variable "slackchannel" {
+  description = "SSM parameter key for the Slack channel. Leave blank if you do not wish to receive Slack notifications."
+  type        = string
+  default     = ""
 }
 
 variable "input_tags" {
--- -tags.tf
+++ -tags.tf
@@ -1,5 +1,5 @@
 locals {
-  tags = merge(var.input_tags, {
-    "ModuleSourceRepo" = "github.com/StratusGrid/terraform-aws-template-terraform-module-starter"
+  common_tags = merge(var.input_tags, {
+    "ModuleSourceRepo" = "github.com/StratusGrid/terraform-aws-limits-monitor"
   })
 }
